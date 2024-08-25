56acc24f07bef1a6c6beacde3bebb0148b9147cb
Fernando Henrique <fernandohsilva@outlook.com.br>
2024-08-25 01:29:09 -0300

    Atualização
    Introdução da biblioteca pandas e tratamento de dados

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ import pandas as pd
 #listando todos os arquivos da pasta vendas list de lista e dir de diretorio/pasta
 lista_arquivo = os.listdir("Vendas")
 
+#criando uma tabela vazia
+
+tabela_total = pd.DataFrame()
+
+
 #Importando a base de dados
+#Explicando o concat: Substituto do append na nova atualização do pandas, o primeiro argumento é a tabela, o segundo argumento o item que será adicionado, e o ignore index true ignora os index individuais dos adicionados e cria uma nova numeração de index, resolvendo possíveis conflitos futuros
 for arquivo in lista_arquivo:
     if "Vendas" in arquivo:
         tabela = pd.read_csv(f"Vendas/{arquivo}")
-        print(arquivo, tabela)
+        tabela_total = pd.concat([tabela_total, tabela], ignore_index=True)
+
+print(tabela_total)
+
 """
 lista_nome=[
     "teste1",
